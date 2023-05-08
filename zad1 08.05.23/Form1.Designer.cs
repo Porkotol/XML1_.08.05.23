55b83aab27e50298c3e77f4d0722acf370b6c576
Чуб Евгений <Zeukacub@gmail.com>
2023-05-09 00:47:18 +0300

    Добавьте файлы проекта.

---
+++ zad1 08.05.23/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace zad1_08._05._23
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(12, 110);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(283, 154);
+            this.propertyGrid1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 88);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(307, 276);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.propertyGrid1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PropertyGrid propertyGrid1;
+        private ListBox listBox1;
+    }
+}
---
+++ zad1 08.05.23/Form1.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+namespace zad1_08._05._23
+{
+    public partial class Form1 : Form
+    {
+        private void LoadEmployees()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("xmltext.xml");
+            foreach (XmlNode node in doc.DocumentElement)
+            {
+                string name = node.Attributes[0].Value;
+                int age = int.Parse(node["Age"].InnerText);
+                bool programmer = bool.Parse(node["Programmer"].InnerText);
+                listBox1.Items.Add(new Employee(name, age, programmer));
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadEmployees();
+     
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1) ;
+            {
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
+            }
+        }
+    }
+}
